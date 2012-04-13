74165f506070de2215c8c0fcf29bf6db4886560a
Mike M <mike.d.meier@gmail.com>
2012-04-13 13:12:21 -0600

    fix indentation/formatting

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -15,20 +15,20 @@ def Start():
 	ObjectContainer.title1 = L('College Humor')
 	ObjectContainer.art = R('art-default.jpg')
 	DirectoryObject.thumb = R('icon-default.png')
-	HTTP.SetCacheTime(CACHE_1HOUR)
+	HTTP.CacheTime = CACHE_1HOUR
 	
 ####################################################################################################
 
 def MainMenu():
 	oc = ObjectContainer()
 	oc.http_cookies = HTTP.CookiesForURL(CH_ROOT)
-    oc.add(DirectoryObject(key=Callback(OriginalsMenu), title="CH Originals"))
-    oc.add(DirectoryObject(key=Callback(ShowMenu, url=CH_ROOT+CH_RECENT, title="Recently Added"), title="Recently Added"))
+	oc.add(DirectoryObject(key=Callback(OriginalsMenu), title="CH Originals"))
+	oc.add(DirectoryObject(key=Callback(ShowMenu, url=CH_ROOT+CH_RECENT, title="Recently Added"), title="Recently Added"))
 	oc.add(DirectoryObject(key=Callback(ShowMenu, url=CH_ROOT+CH_VIEWED, title="Most Viewed"), title="Most Viewed"))
-    oc.add(DirectoryObject(key=Callback(VideoPlaylistsMenu, url=CH_ROOT+CH_VIDEO_PLAYLISTS), title="Video Playlists"))
-    oc.add(DirectoryObject(key=Callback(SketchMenu, url=CH_ROOT+CH_SKETCH), title="Sketch Comedy"))
+	oc.add(DirectoryObject(key=Callback(VideoPlaylistsMenu, url=CH_ROOT+CH_VIDEO_PLAYLIST), title="Video Playlists"))
+	oc.add(DirectoryObject(key=Callback(SketchMenu, url=CH_ROOT+CH_SKETCH), title="Sketch Comedy"))
 	return oc
-    
+
 ####################################################################################################
 			
 def OriginalsMenu():
@@ -37,50 +37,53 @@ def OriginalsMenu():
 	for show in HTML.ElementFromURL(CH_ROOT + '/videos').xpath('//div[@class="sidebar_nav"]/ul[2]/li/a')[:-1]:
 		url = CH_ROOT + show.get('href')
 		title = show.text
-        oc.add(DirectoryObject(key=Callback(ShowMenu, url=url, title=title), title=title))
+		oc.add(DirectoryObject(key=Callback(ShowMenu, url=url, title=title), title=title))
 	return oc
-    
+
 ####################################################################################################
 	
 def VideoPlaylistsMenu(url):
-    oc = ObjectContainer(title2="Video Playlists")
+	oc = ObjectContainer(title2="Video Playlists")
 	for item in HTML.ElementFromURL(url).xpath("//div[@class='media video playlist horizontal']"):
 		title = item.xpath('./a')[0].get('title')
 		summary = item.xpath('./div/p')[0].text
 		thumbURL = item.xpath("a/img")[0].get('src')
-		videoURL = CH_ROOT + CH_VIDEO_PLAYLIST + item.xpath('a')[0].get('href'))
-        oc.add(DirectoryObject(key=Callback(ShowMenu, url=videoURL, title=title), title=title,
-            thumb=Resource.ContentsOfURLWithFallback(self, url=thumbURL, fallback='icon-default.png', summary=summary))
+		videoURL = CH_ROOT + CH_VIDEO_PLAYLIST + item.xpath('a')[0].get('href')
+		oc.add(DirectoryObject(key=Callback(ShowMenu, url=videoURL, title=title), title=title,
+			thumb=Resource.ContentsOfURLWithFallback(self, url=thumbURL, fallback='icon-default.png', summary=summary)))
+	
 	next = getNext(url, VideoPlaylistsMenu)
 	if next != None: oc.add(next)
 	return oc
-    
+
 ####################################################################################################
 	
 def SketchMenu(url):
-    oc = ObjectContainer(title2="Sketch Comedy")
+	oc = ObjectContainer(title2="Sketch Comedy")
 	for item in HTML.ElementFromURL(url).xpath("//div[@class='media horizontal sketch_group']"):
 		title = item.xpath('./a')[0].get('title')
-		videoURL = url + item.xpath('./a')[0].get('href'))
+		videoURL = url + item.xpath('./a')[0].get('href')
 		summary = item.xpath('./div[@class="details"]/p')[0].text.strip()
 		thumbURL = item.xpath('./a/img')[0].get('src')
-        oc.add(DirectoryObject(key=Callback(ShowMenu, url=videoURL, title=title), title=title, summary=summary,
-            thumb=Resource.ContentsOfURLWithFallback(self, url=thumbURL, fallback='icon-default.png'))
+		oc.add(DirectoryObject(key=Callback(ShowMenu, url=videoURL, title=title), title=title, summary=summary,
+			thumb=Resource.ContentsOfURLWithFallback(self, url=thumbURL, fallback='icon-default.png')))
+	
 	next = getNext(url, SketchMenu)
 	if next != None: oc.add(next)
 	return oc
-    
+
 ####################################################################################################
 
 def ShowMenu(url, title=''):  
-    oc = ObjectContainer(title2=title)
+	oc = ObjectContainer(title2=title)
 	for item in HTML.ElementFromURL(url).xpath('//div[@class="media video horizontal"]'):
 		title = item.xpath('./a')[0].get('title')
 		itemURL = item.xpath('./a')[0].get('href')
 		summary = item.xpath('./div[@class="details"]/p')[0].text.strip()
 		thumbURL = item.xpath('./a/img')[0].get('src')
-        oc.add(VideoClipObject(url=itemURL, title=title, summary=summary, 
-            thumb=Resource.ContentsOfURLWithFallback(self, url=thumbURL, fallback='icon-default.png'))
+		oc.add(VideoClipObject(url=itemURL, title=title, summary=summary, 
+			thumb=Resource.ContentsOfURLWithFallback(self, url=thumbURL, fallback='icon-default.png')))
+	
 	next = getNext(url, ShowMenu)
 	if next != None: oc.add(next)
 	return oc
@@ -88,8 +91,8 @@ def ShowMenu(url, title=''):
 ####################################################################################################
 
 def getNext(url, menu):
-    next = HTML.ElementFromURL(url).xpath('//a[@class="next"]')
+	next = HTML.ElementFromURL(url).xpath('//a[@class="next"]')
 	if len(next) != 0:
-		return Function(DirectoryItem(menu, title='Next', thumb=R('Next.png')), url=CH_ROOT + next[0].get('href'))
-    else:
-        return None
+		return (DirectoryObject(key=Callback(menu, url=CH_ROOT + next[0].get('href')), title='Next', thumb=R('Next.png')))
+	else:
+		return None
